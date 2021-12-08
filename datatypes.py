a1687f6e0e7e3d88a76918572e2793fd90523c34
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-08 20:54:51 +0100

    Delete datatypes.py

--- datatypes.py
+++
@@ -1,8 +0,0 @@
-class class_:
-
-    def __init__(self, name, base, attrs):
-        self._make(name, base, attrs)
-
-
-    def make(self, name, base, attrs, methods):
-        return type(name, base, attrs)
