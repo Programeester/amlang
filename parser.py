fe084bea9b7343b002f598fd62ac4519d9900c16
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-09 11:05:34 +0100

    Delete parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-import lexer
-from os.path import exists, isfile 
-
-def is_valid_path(path):
-    if lexer.patterns["path_pattern"].match(path) and exists(path) and isfile(path):
-        return True
-
-def parse(file):
-    pass
