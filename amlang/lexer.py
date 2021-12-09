885a468fbbf9c3ae61bd50f7727508672f83bf9e
programmeester <87316911+Programeester@users.noreply.github.com>
2021-12-09 12:44:13 +0100

    Update lexer.py

--- amlang/lexer.py
+++ amlang/lexer.py
@@ -31,19 +31,22 @@ keywords = {
     "pass" : "PASS",
     "break" : "BREAK",
     "func" : "FUNC",
+}
+
+line_filling_keywords = {
     "raar" : "RAAR",
     "ettol" : "ETTOL",
     "ray" : "RAY",
 }
 
 data_types = {
-    "getal" : dt.getal,
-    "string" : dt.string,
-    "list" : dt.list,
-    "dict" : dt.dictionary,
+    r"\d+" : dt.getal,
+    r"[\d\w\s\t\f]*" : dt.string,
+    r"\[([\d\w\s\t\f]*,)*\]" : dt.list,
 }
 
 patterns = {
     "path_pattern" : re.compile(r".*\.uh"),  #  controleerd of het bestand eindigd op de goede extensie
-    "keyword_pattern" : re.compile(rf"[{keywords.keys()}]([{data_types.keys()}][])")
+    "keyword_pattern" : re.compile(rf"[{keywords.keys()}]([{data_types.keys()}][])")  #  kijken voor keywords
+    "comparison_pattern" : re.compile(rf"([{data_types.keys()}][])\s[{comparison_characters.keys()}]\s([{data_types.keys()}][])")
 }
